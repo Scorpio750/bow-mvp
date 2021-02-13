7120f34cc09dce0c04ab3a5cf830fa5519c79592
Sascha Wu <sascha.wu@protonmail.com>
2021-02-12 19:10:41 -0500

    folder footer into react-router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,141 @@
-import logo from './assets/body of workers.png';
 import './App.css';
 
-import Footer from './components/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { LoremIpsum } from 'lorem-ipsum';
 
-function App() {
+import Home from './containers/Home/Home';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Enter.
-        </a>
-      </header>
-      <Footer />
+    <Router>
+      <div className="App">
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/FAQ">
+            <FAQ />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/tandc">
+            <TandC />
+          </Route>
+          <Route path="/privacy">
+            <Privacy />
+          </Route>
+          <Route path="/donations">
+            <Donations />
+          </Route>
+          <Route path="/mailing">
+            <MailingList />
+          </Route>
+        </Switch>
+
+        <footer>
+          <ul>
+            <li>
+              <Link to="/about">ABOUT</Link>
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+            <li>
+              <Link to="/contact">CONTACT</Link>
+            </li>
+            <li>
+              <Link to="/tandc">TERMS AND CONDITIONS</Link>
+            </li>
+            <li>
+              <Link to="/privacy">PRIVACY</Link>
+            </li>
+            <li>
+              <Link to="/donations">DONATIONS</Link>
+            </li>
+            <li>
+              <Link to="/mailing">MAILING LIST</Link>
+            </li>
+    </ul>
+    </footer>
     </div>
+    </Router>
   );
 }
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+);
+
+const FAQ = () => (
+  <div>
+    <h1>FAQ</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+);
+
+const Contact = () => (
+  <div>
+    <h1>Contact</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+)
+
+const TandC = () => (
+  <div>
+    <h1>TandC</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+);
+
+const Privacy = () => (
+  <div>
+    <h1>Privacy</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+);
+
+const Donations = () => (
+  <div>
+    <h1>Donations</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+);
+
+const MailingList = () => (
+  <div>
+    <h1>Mailing List</h1>
+    <p>{lorem.generateParagraphs(4)}</p>
+  </div>
+);
 
 export default App;
---
+++ react-app/src/containers/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../assets/body of workers.png';
+
+export const Home = props => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <a
+      className="App-link"
+      href="https://reactjs.org"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Enter.
+    </a>
+  </header>
+);
+
+export default Home;
