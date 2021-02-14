da4a60be1c9dac9ebe97bf6d902cfec2d9156c6a
Kadecahe <kadecahe@gmail.com>
2021-02-14 10:59:33 -0500

    Feat: Server running with script npm run start-dev

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const compression = require('compression');
+//For potential user sessions and Oauth
 // const session = require('express-session');
 // const passport = require('passport');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(morgan('dev'));
@@ -17,39 +18,40 @@ app.use(express.urlencoded({extended: true}));
 app.use(compression());
 
 //Static Middleware
-// app.use(express.static(path.join(__dirname, 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, 'build', 'index.html')));
 
 //api routes
-app.get('/api/ping', function (req, res) {
+app.get('/ping', function (req, res) {
   return res.send('pong');
  });
 
-app.use('/api', require('./api'));
-app.use('/auth', require('./auth'));
+ //For future Auth or Auth routes
+// app.use('/api', require('./api'));
+// app.use('/auth', require('./auth'));
 
 
 // any remaining requests with an extension (.js, .css, etc.) send 404
-// app.use((req, res, next) => {
-//   if (path.extname(req.path).length) {
-//     const err = new Error('Not found')
-//     err.status = 404
-//     next(err)
-//   } else {
-//     next()
-//   }
-// })
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
 
-// // sends index.html
-// app.use('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-// })
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
 
-// // error handling endware
-// app.use((err, req, res, next) => {
-//   console.error(err)
-//   console.error(err.stack)
-//   res.status(err.status || 500).send(err.message || 'Internal server error.')
-// })
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
 
 
 app.listen(PORT, () => {
