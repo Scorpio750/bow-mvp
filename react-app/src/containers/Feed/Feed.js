1eef8094ef208bbde273a1a3bf495b1f9f2e2ed5
Sascha Wu <sascha.wu@protonmail.com>
2021-02-14 21:53:36 -0500

    add styling for feed columns

--- react-app/src/containers/Feed/Feed.js
+++ react-app/src/containers/Feed/Feed.js
@@ -6,24 +6,39 @@ import { getAllPosts } from '../../store/actions/posts';
 
 import styles from './Feed.module.css';
 
-export const renderPostPreviews = posts => {
+export const renderPostPreviews = (posts, colIndex) => {
   posts = ['hi', 'hi', 'hi', 'hi', 'kinky'];
-  return (posts.map(post => {
-    <li>hello</li>
-  }));
+  const moddedPosts = posts.reduce((modList, post, index) => {
+    if (index % 3 === colIndex) modList.push(post);
+    return modList;
+  }, []);
+  const renderedPosts = moddedPosts.map(post => <li key={Math.random()}>{post}</li>);
+  return renderedPosts;
 };
 
 export const Feed = props => {
   useEffect(() => {
-    console.log(props);
+    console.log({ props });
     props.getAllPosts();
   });
 
   return (
     <div className={styles.feedContainer}>
-      <ul className={styles.postsList}>
-        {renderPostPreviews(props.posts)}
-      </ul>
+      <div className={styles.feedColumn1}>
+        <ul className={styles.postsList}>
+          {renderPostPreviews(props.posts, 0)}
+        </ul>
+      </div>
+      <div className={styles.feedColumn2}>
+        <ul className={styles.postsList}>
+          {renderPostPreviews(props.posts, 1)}
+        </ul>
+      </div>
+      <div className={styles.feedColumn3}>
+        <ul className={styles.postsList}>
+          {renderPostPreviews(props.posts, 2)}
+        </ul>
+      </div>
     </div>
   )
 }
