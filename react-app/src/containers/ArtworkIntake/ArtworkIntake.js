cda2d498e095a81241ae1747ec4dc9c8ac1cad71
Sascha Wu <sascha.wu@protonmail.com>
2021-02-17 03:36:50 -0500

    add image preview, clean up styling, written work preview

--- react-app/src/containers/ArtworkIntake/ArtworkIntake.js
+++ react-app/src/containers/ArtworkIntake/ArtworkIntake.js
@@ -1,161 +1,206 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { useForm } from 'react-hook-form';
 
-import styles from '../../common/Form.module.css';
+import formStyles from '../../common/Form.module.css';
+import aiStyles from './ArtworkIntake.module.css';
+import placeholder from '../../assets/sexy_placeholder.jpg';
 
 import { createPost } from '../../store/actions/posts';
 
 export const ArtworkIntake = props => {
+  const [imagePreview, setImagePreview] = useState(null);
+  const [showWrittenWork, setShowWrittenWork] = useState(false);
+
   const { register, handleSubmit, watch, errors } = useForm();
+
+  const fileChangedHandler = e => {
+    e.preventDefault();
+
+    let reader = new FileReader();
+
+    reader.onloadend = () => setImagePreview(reader.result);
+
+    e.target.files[0] && reader.readAsDataURL(e.target.files[0])
+  };
+
+  const showWrittenWorkBodyHandler = e => {
+    const result = e.target.value === "yes" ? true : false;
+    setShowWrittenWork(result);
+  }
+
+  const renderWrittenWorkBody = () => (
+    <div class={aiStyles.writtenWorkBodyContainer}>
+      <textarea
+        className={`${aiStyles.writtenWorkBody} ${formStyles.textarea}`}
+        name="written-work-body"
+        rows="8"
+        cols="90"
+        placeholder="paste your work here..."
+      />
+    </div>
+  );
+
   const onSubmit = async data => {
-    await axios.post('/api/user/login', data)
+    await axios.post('/api/user/create-post', data)
   };
 
   return (
-    <div className={styles.formContainer}>
-      <h1 className={styles.header}>Artwork Intake</h1>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+    <div className={formStyles.formContainer}>
+      <h1 className={formStyles.header}>Artwork Intake</h1>
+      <form className={formStyles.form} onSubmit={handleSubmit(onSubmit)}>
         {/* register your input into the hook by invoking the "register" function */}
-        <label for="file">Upload your work here (you may select multiple at a time)</label>
-        <input
-          className={styles.input}
-          name="file"
-          type="file"
-        />
-        <input
-          name="title"
-          className={styles.input}
-          placeholder="title"
-          ref={register({ required: true })}
-        />
-        {errors.title && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="year"
-          className={styles.input}
-          placeholder="Year"
-          ref={register({ required: true })}
-        />
-        {errors.year && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="medium"
-          className={styles.input}
-          placeholder="medium"
-          ref={register({ required: true })}
-        />
-        {errors.medium && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="genre"
-          className={styles.input}
-          placeholder="genre"
-          ref={register({ required: true })}
-        />
-        {errors.genre && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="tags"
-          className={styles.input}
-          placeholder="#tag1, #tag2, #tag3..."
-          ref={register({ required: true })}
-        />
-        {errors.tags && <span className={styles.error}>This field is required</span>}
-
-        <textarea
-          name="caption"
-          className={styles.textarea}
-          placeholder="caption"
-          ref={register({ required: true })}
-        />
-        {errors.caption && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="languages"
-          className={styles.input}
-          placeholder="languages"
-          ref={register({ required: true })}
-        />
-        {errors.languages && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="credits"
-          className={styles.input}
-          placeholder="credits"
-          ref={register({ required: true })}
-        />
-        {errors.credits && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="distributor"
-          className={styles.input}
-          placeholder="distributor"
-          ref={register({ required: true })}
-        />
-        {errors.distributor && <span className={styles.error}>This field is required</span>}
-
-        <input
-          name="press"
-          className={styles.input}
-          placeholder="press"
-          ref={register({ required: true })}
-        />
-        {errors.press && <span className={styles.error}>This field is required</span>}
-
-        <div className={styles.radioContainer}>
-          <h3>Is this content safe for work?</h3>
+        <div className={formStyles.filepicker}>
+          <label htmlFor="file">Upload your work here.</label>
           <input
-            name="sfw"
-            type="radio"
-            className={styles.input}
-            id="sfw"
-            value="sfw"
-            ref={register({ required: true })}
+            className={formStyles.upload}
+            id="file"
+            name="file"
+            type="file"
+            onChange={fileChangedHandler}
           />
-          <label for="sfw">Yes</label>
-          <input
-            name="sfw"
-            type="radio"
-            className={styles.input}
-            id="nsfw"
-            value="nsfw"
-            ref={register({ required: true })}
-          />
-          <label for="nsfw">No</label>
-        </div>
+          <div className={formStyles.previewContainer}>
+            <img
+              src={imagePreview || placeholder}
+              alt="waiting for your beautiful art here <3"
+            />
+          </div>
 
-        <div className={styles.radioContainer}>
-          <h3>Is this a written work?</h3>
-          <input
-            name="written"
-            type="radio"
-            className={styles.input}
-            id="written"
-            value="yes"
-            ref={register({ required: true })}
-          />
-          <label for="written">Yes</label>
-          <input
-            name="written"
-            type="radio"
-            className={styles.input}
-            id="nonwritten"
-            value="no"
-            ref={register({ required: true })}
-          />
-          <label for="nsfw">No</label>
           <textarea
-            name="writtenWorkBody"
-            className={styles.textarea}
-            placeholder="If so, please paste into the area below"
+            name="caption"
+            className={formStyles.textarea}
+            placeholder="caption"
+            ref={register()}
           />
         </div>
 
-        <input className={styles.submit} type="submit" />
+        <div className={formStyles.formSectionWrapper}>
+          <section className={formStyles.formSection}>
+            <input
+              name="title"
+              className={formStyles.input}
+              placeholder="title"
+              ref={register({ required: true })}
+            />
+            {errors.title && <span className={formStyles.error}>This field is required</span>}
+
+            <input
+              name="year"
+              className={formStyles.input}
+              placeholder="year"
+              ref={register()}
+            />
+
+            <input
+              name="medium"
+              className={formStyles.input}
+              placeholder="medium"
+              ref={register()}
+            />
+
+            <input
+              name="genre"
+              className={formStyles.input}
+              placeholder="genre"
+              ref={register()}
+            />
+
+            <input
+              name="tags"
+              className={formStyles.input}
+              placeholder="#tag1 #tag2 #tag3 ..."
+              ref={register()}
+            />
+          </section>
+          <section className={formStyles.formSection}>
+            <input
+              name="languages"
+              className={formStyles.input}
+              placeholder="language 1, language 2 ..."
+              ref={register()}
+            />
+
+            <input
+              name="credits"
+              className={formStyles.input}
+              placeholder="credits"
+              ref={register()}
+            />
+
+            <input
+              name="distributor"
+              className={formStyles.input}
+              placeholder="distributor"
+              ref={register()}
+            />
+
+            <input
+              name="press"
+              className={formStyles.input}
+              placeholder="press"
+              ref={register()}
+            />
+          </section>
+        </div>
+
+        <div className={formStyles.formSectionWrapper}>
+          <section className={formStyles.formSection}>
+            <div className={formStyles.radioContainer}>
+              <h4>Is this content safe for work?</h4>
+              <input
+                name="sfw"
+                type="radio"
+                className={formStyles.radio}
+                id="sfw"
+                value="sfw"
+                ref={register({ required: true })}
+              />
+              <label htmlFor="sfw">Yes</label>
+              <input
+                name="sfw"
+                type="radio"
+                className={formStyles.radio}
+                id="nsfw"
+                value="nsfw"
+                ref={register({ required: true })}
+              />
+              <label htmlFor="nsfw">No</label>
+            </div>
+          </section>
+          <section className={formStyles.formSection}>
+            <div className={formStyles.radioContainer}>
+              <h4>Is this a written work?</h4>
+              <input
+                name="written"
+                type="radio"
+                className={formStyles.radio}
+                id="written"
+                value="yes"
+                ref={register({ required: true })}
+                onChange={showWrittenWorkBodyHandler}
+              />
+              <label htmlFor="written">Yes</label>
+              <input
+                name="written"
+                type="radio"
+                className={formStyles.radio}
+                id="nonwritten"
+                value="no"
+                defaultChecked
+                ref={register({ required: true })}
+                onChange={showWrittenWorkBodyHandler}
+              />
+              <label htmlFor="nsfw">No</label>
+            </div>
+          </section>
+        </div>
+
+        {showWrittenWork && renderWrittenWorkBody()}
+
+        <input className={formStyles.submit} type="submit" />
       </form>
-      <div className={styles.additionalActionsContainer}>
+      <div className={formStyles.additionalActionsContainer}>
         <p>Upload another work</p>
       </div>
     </div>
