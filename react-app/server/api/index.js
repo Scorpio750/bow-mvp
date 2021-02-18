9f4f45858291e19153cbb75e95398c00e9bf7679
Kade Cahe <55356085+Kadecahe@users.noreply.github.com>
2021-02-18 00:56:13 -0500

    Crated Post Schema (#25)

--- react-app/server/api/index.js
+++ react-app/server/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 module.exports = router;
-
+// '/api'
 router.use('/user', require('./user'))
+router.use('/post', require('./post'))
+
--- react-app/server/db/models/post.js
+++ react-app/server/db/models/post.js
@@ -2,10 +2,45 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Post = db.define('post', {
-  name: {
-    type: Sequelize.STRING
-  }
-})
+  title: {
+    type:  Sequelize.STRING,
+    allowNull: null,
+    validate: {
+      notEmpty: true,
+    }
+  },
+  image: Sequelize.BLOB,
+  sequence: Sequelize.INT,
+  // artistFK: Sequelize.INT,
+  privacy: Sequelize.INT,
+  //if majority says ok, pending bridget res
+  canMarket: {
+    type: Sequelize.BOOL,
+    defaultValue: false
+  },
+  sfw: {
+    type: Sequelize.BOOL,
+    defaultValue: false
+  },
+  caption: Sequelize.STRING,
+  price: Sequelize.FLOAT,
+  year: Sequelize.DATE,
+  medium: Sequelize.STRING,
+
+  materials: Sequelize.STRING,
+  dimensions: Sequelize.STRING,
+  location: Sequelize.STRING,
+  geo: Sequelize.STRING,
+  genre: Sequelize.STRING,
+  subset: Sequelize.STRING,
+  language: Sequelize.STRING,
+  credits: Sequelize.STRING,
+  press: Sequelize.STRING,
+  distributor: Sequelize.STRING,
+  tags: Sequelize.ARRAY(Sequelize.STRING),
+}
+
+)
 
 
 module.exports = Post
