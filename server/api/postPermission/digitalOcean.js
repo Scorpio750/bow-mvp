07a4c2d44ad2deca880160e5bb39cfcba459c6b0
Kadecahe <kadecahe@gmail.com>
2021-02-19 18:40:46 -0500

    Code block for DO api

---
+++ server/api/postPermission/digitalOcean.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const AWS = require('aws-sdk');
+//if we app a router, this is mounted on /api/do
+
+//This configuration allows us to reach the required endpoint. nyc3 is our Region
+const artistEndpoint = new AWS.Endpoint('https://nyc3.digitaloceanspaces.com');
+const s3 = new AWS.S3({
+  endpoint: artistEndpoint,
+  //We need both a key and secret to access any endpoint
+  accessKeyId: process.env.DO_SPACES_PUBLIC,
+  secretAccessKey: process.env.DO_SPACES_SECRET
+});
+
+
+const params = {
+  //The Bucket key points to the DO Space that we are using called bodyofworkers
+  Bucket: "bodyofworkers",
+  //The Key refers to the title of the file/object that we wat to access. Let's think about how to use the Key dynamically
+  // Key: "Domina_Mara_IMG_4036-sm.png"
+};
+
+//fetches a single Object based on the params passed it.
+// s3.getObject(params, function(err, data) {
+//   if (err) console.log(err, err.stack);
+//   else {
+
+//       console.log(data)
+//       //the buffer object lives here: data.Body
+//       // console.log(data.Body)
+//   };
+// });
+
+module.exports = {
+  params,
+  s3
+}
--- server/api/postPermission/findAllPublic.js
+++ server/api/postPermission/findAllPublic.js
@@ -1,11 +1,73 @@
-
+const {params, s3 } = require('./digitalOcean')
 async function findAllPublic(artwork) {
 
 
   const post = artwork.map((currPost) => {
 
 
-    if(currPost.privacy === 1) return currPost
+    if(currPost.privacy === 1) {
+      let {
+        title,
+        sequence,
+        fileName,
+        caption,
+        instagram,
+        twitter,
+        microBio,
+        location,
+        geo,
+        user,
+        medium,
+        materials,
+        dimensions,
+        genre,
+        languages,
+        references,
+        credits,
+        distributor,
+        pressLink,
+        tags
+      } = currPost;
+      let newFile = {
+        title,
+        fileName,
+        //file
+        sequence,
+        caption,
+        instagram,
+        twitter,
+        microBio,
+        location,
+        geo,
+        user,
+        medium,
+        materials,
+        dimensions,
+        genre,
+        languages,
+        references,
+        credits,
+        distributor,
+        pressLink,
+        tags
+      }
+
+      params.Key = fileName
+
+      const res = s3.getObject(params, function (err, data) {
+        if(err) console.log(err, err.stack);
+        else {
+          //this is the data we must put somewhere.
+          console.log(data.ContentType, data.Body)
+
+        }
+      })
+
+      console.log(res)
+
+      return newFile
+
+    }
 
     else {
       let {
