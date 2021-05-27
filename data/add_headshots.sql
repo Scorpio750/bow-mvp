24e3cf11b874047782280800da4ec8d1bbbeba73
Sascha Wu <sascha.wu@protonmail.com>
2021-05-27 05:26:50 -0400

    add artist headshot

---
+++ data/add_headshots.sql
@@ -0,0 +1,42 @@
+ALTER TABLE users
+ADD COLUMN artist_headshot TEXT;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_domina_mara.jpg'
+WHERE id=3;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_empress_wu.jpg'
+WHERE id=4;
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_lena_chen.jpg'
+WHERE id=9;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_Lilith.jpg'
+WHERE id=10;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_LP-2.jpg'
+WHERE id=18;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_Mars.jpg'
+WHERE id=13;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_red.jpg'
+WHERE id=15;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_squiggles_and_sluts.jpg'
+WHERE id=19;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_yin_q.jpg'
+WHERE id=16;
+
+UPDATE users
+SET artist_headshot='https://bodyofworkers.nyc3.digitaloceanspaces.com/artist-headshots/headshot_ze_royale.jpg'
+WHERE id=17;
+
